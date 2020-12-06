f8f61b2efd07df2ab345bbc213e858ecd5644de6
wpj980517 <2448163637@qq.com>
2020-12-06 14:34:11 +0800

    更新1

--- HelloGit/src/main/java/com/wpj/git.java
+++ HelloGit/src/main/java/com/wpj/git.java
@@ -5,5 +5,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
